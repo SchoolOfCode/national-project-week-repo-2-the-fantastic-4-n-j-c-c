f3f9c2fd1e48779361ac8b4212c9dcccf48d4721
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-01-18 16:05:08 +0000

    front end folders copied over from back-end repo

---
+++ random-name-generator/src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Name from "../Name";
+
+
+function Card() {
+return <div>
+    <Name text="Cigdem"/>
+</div>
+}
+
+export default Display;
---
+++ random-name-generator/src/components/Name/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Name({text}) {
+return <h1>{text}</h1>
+}
+
+export default Name;
