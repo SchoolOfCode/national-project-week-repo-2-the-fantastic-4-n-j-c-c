feb5d7ffa72b9e0137817a880a9238cf0ada4096
CigdemOzturk <cigdemozturk_@hotmail.com>
2022-01-19 10:44:35 +0000

    Some changes made

--- random-name-generator/src/components/Button/index.js
+++ random-name-generator/src/components/Button/index.js
@@ -2,39 +2,39 @@ import React, { useEffect, useState } from "react";
 
 function Button({ onClick }) {
   const [bootcamperName, setName] = useState("");
-  const [bootcamperNum, setBootcamperNum] = useState(0);
+  // const [bootcamperNum, setBootcamperNum] = useState(0);
 
-  async function changeIsChosen() {
-    const response = await fetch(
-      `https://socnamegenerator.herokuapp.com/bootcampers/${bootcamperNum}`,
-      {
-        method: "PATCH",
-        body: JSON.stringify({
-          isChosen: true,
-        }),
-      }
-    );
-  }
+  // async function changeIsChosen() {
+  //   await fetch(
+  //     `https://socnamegenerator.herokuapp.com/bootcampers/${bootcamperNum}`,
+  //     {
+  //       method: "PATCH",
+  //       body: JSON.stringify({
+  //         isChosen: true,
+  //       }),
+  //     }
+  //   );
+  // }
 
   async function getRandomBootcampers() {
     const num = Math.floor(Math.random() * 10 + 1);
-    setBootcamperNum(num);
+    // setBootcamperNum(num);
     const response = await fetch(
       `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
     );
     let data = await response.json();
     console.log(data);
-    while (data.payload[0].ischosen) {
-      const num = Math.floor(Math.random() * 10 + 1);
-      const response = await fetch(
-        `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
-      );
-      data = await response.json();
-    }
+    // while (data.payload[0].ischosen) {
+    //   const num = Math.floor(Math.random() * 10 + 1);
+    //   const response = await fetch(
+    //     `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
+    //   );
+    //   data = await response.json();
+    // }
 
     console.log(data.payload[0].firstname);
     const name = data.payload[0].firstname;
-    changeIsChosen();
+    // changeIsChosen();
     setName(name);
   }
 
