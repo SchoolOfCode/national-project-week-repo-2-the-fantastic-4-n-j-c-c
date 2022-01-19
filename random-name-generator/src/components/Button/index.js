8e39aecbcf2ed63760fcf557011b410579b8c8c4
Chun Wai <lochunwai5@gmail.com>
2022-01-19 10:24:01 +0000

    changeIsChosen function created

--- random-name-generator/src/components/Button/index.js
+++ random-name-generator/src/components/Button/index.js
@@ -2,24 +2,39 @@ import React, { useEffect, useState } from "react";
 
 function Button({ onClick }) {
   const [bootcamperName, setName] = useState("");
+  const [bootcamperNum, setBootcamperNum] = useState(0);
+
+  async function changeIsChosen() {
+    const response = await fetch(
+      `https://socnamegenerator.herokuapp.com/bootcampers/${bootcamperNum}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          isChosen: true,
+        }),
+      }
+    );
+  }
 
   async function getRandomBootcampers() {
     const num = Math.floor(Math.random() * 10 + 1);
+    setBootcamperNum(num);
     const response = await fetch(
       `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
     );
     let data = await response.json();
-    console.log(data)
-    while (data.payload[0].ischosen){
+    console.log(data);
+    while (data.payload[0].ischosen) {
       const num = Math.floor(Math.random() * 10 + 1);
-    const response = await fetch(
-      `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
-    );
-    data = await response.json();
+      const response = await fetch(
+        `https://socnamegenerator.herokuapp.com/bootcampers/${num}`
+      );
+      data = await response.json();
     }
 
     console.log(data.payload[0].firstname);
     const name = data.payload[0].firstname;
+    changeIsChosen();
     setName(name);
   }
 
