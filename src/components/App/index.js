d9d0413f9ef9be9de02d526d529094049b54c72e
CigdemOzturk <cigdemozturk_@hotmail.com>
2022-01-20 14:58:44 +0000

    It works

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -69,62 +69,81 @@ const nameList = [
 function App() {
   const [bootcampers, setBootcampers] = useState(nameList);
   const [currentIndex, setCurrentIndex] = useState(4);
-  
-  // useEffect(() => {
-  //   async function getData() {
-  //     const response = await fetch(`https://socnamegenerator.herokuapp.com/bootcampers`)
-  //     const json = await response.json();
-  //     const {payload} = json;
-  //     setBootcampers(payload)
-  //     const randNum = Math.floor(Math.random() * (payload.length - 1 ));
-  //     setCurrentIndex(randNum)
-  //   }
-  //   getData();
-  // }, [])
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `https://socnamegenerator.herokuapp.com/bootcampers`
+      );
+      const json = await response.json();
+      const { payload } = json;
+      setBootcampers(payload);
+      const randNum = Math.floor(Math.random() * (payload.length - 1));
+      setCurrentIndex(randNum);
+    }
+    getData();
+  }, []);
 
   function getNewBootcamper() {
-
-  //change the ischosen from false to true if selected
+    //change the ischosen from false to true if selected
     //unchosenBootcampers array
     //useState (setBootcampers)
-      //spread the original bootcampers array (THIS IS CAUSING AN INFINITE LOOP)
-        //This needs to be wrapped in a useEffect
-        //select only the bootcamper with the id (idOfUnchosenBootcamper)
-        //select the ischosen key within that bootcamper(object)
-        //update the ischosen key to be true.
-console.log("original list", bootcampers);
-      const bootcampersUpdated = bootcampers.map((bootcamper, i) => {
-        if (i===currentIndex) {
-          return { ...bootcamper, ischosen:true }
-        } 
-        return bootcamper;
-      })
+    //spread the original bootcampers array (THIS IS CAUSING AN INFINITE LOOP)
+    //This needs to be wrapped in a useEffect
+    //select only the bootcamper with the id (idOfUnchosenBootcamper)
+    //select the ischosen key within that bootcamper(object)
+    //update the ischosen key to be true.
+    console.log("original list", bootcampers);
+    const bootcampersUpdated = bootcampers.map((bootcamper, i) => {
+      if (i === currentIndex) {
+        return { ...bootcamper, ischosen: true };
+      }
+      return bootcamper;
+    });
 
     const unchosenBootcampers = bootcampersUpdated.filter((bootcamper) => {
       return bootcamper.ischosen === false;
     });
-    const randNum = Math.floor(Math.random() * unchosenBootcampers.length);
 
-    const idOfUnchosenBootcamper = unchosenBootcampers[randNum].id;
-    const chosen = bootcampers.findIndex(
-      (b) => b.id === idOfUnchosenBootcamper
-    );
-    setCurrentIndex(chosen);
-    setBootcampers(bootcampersUpdated);
-  console.log("updated list", bootcampers)
+    //if we have no more unchosen bootcampers
+    if (unchosenBootcampers.length === 0) {
+      // reset list
+      const bootcampersReset = bootcampers.map((bootcamper) => {
+        return { ...bootcamper, ischosen: false };
+      });
+      // set random index
+      const randNum = Math.floor(Math.random() * (bootcampersReset.length - 1));
+      setCurrentIndex(randNum);
+      setBootcampers(bootcampersReset);
+      //if we still have unchosen bootcampers
+    } else {
+      console.log("UNCHOSENBOOTCAMPER: ", unchosenBootcampers);
+      const randNum = Math.floor(Math.random() * unchosenBootcampers.length);
+      console.log("RANDOM NUMBER: ", randNum);
+      const idOfUnchosenBootcamper = unchosenBootcampers[randNum].id;
+      const chosen = bootcampers.findIndex(
+        (b) => b.id === idOfUnchosenBootcamper
+      );
+      setCurrentIndex(chosen);
+      setBootcampers(bootcampersUpdated);
+      console.log("updated list", bootcampers);
+    }
   }
 
   return (
     <div className="App">
-      {/* {bootcampers.length > 0 && currentIndex >= 0 &&  */}
       <div className="title">
         <h1>School of Code Name Generator</h1>
       </div>
-      <Card
-        bootcamper={bootcampers[currentIndex]}
-        chooseNewBootcamper={getNewBootcamper}
-      />
-      {/* } */}
+      {bootcampers.length > 0 &&
+        currentIndex >= 0 &&
+        currentIndex !== "undefined" &&
+        bootcampers.length !== "undefined" && (
+          <Card
+            bootcamper={bootcampers[currentIndex]}
+            chooseNewBootcamper={getNewBootcamper}
+          />
+        )}
     </div>
   );
 }
