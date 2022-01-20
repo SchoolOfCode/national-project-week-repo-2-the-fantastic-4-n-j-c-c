cf605004e0ce5ebbf664b5b951c24a4986fb918e
Chun Wai <lochunwai5@gmail.com>
2022-01-20 16:09:46 +0000

    coin flip plan

--- src/components/Name/index.js
+++ src/components/Name/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Name.css";
 
 function Name({ text }) {
-  return <h1 className="name">{text}</h1>;
+  return <h2 className="name">{text}</h2>;
 }
 
 export default Name;
